cf81af7a0bfb53c057bf1f049b7e2ca414aea04a
daniel <danielkolotashvilio@gmail.com>
2024-12-19 12:41:23 +0400

    week 13 ??

---
+++ src/main/java/ge/ibsu/demo/dto/AddAddress.java
@@ -0,0 +1,14 @@
+package ge.ibsu.demo.dto;
+
+public class AddAddress {
+
+    private String address;
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address= address;
+    }
+}
